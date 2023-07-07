ed3f0042ff57646430bd80fdba320a122e5c3ca6
Shoaib Latif <shoaiblatif11@gmail.com>
2023-07-07 14:40:47 +0100

    Added in css and navbar components to frontend

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -5,10 +5,14 @@ import SinglePost from "../singlePost/SinglePost";
 import React, { useState } from "react";
 import Feed from "../feed/Feed";
 import { useNavigate, Routes, Route } from "react-router-dom";
+import Navbar from './navbar.js';
+import './navbar.css';
 
 
 const App = () => {
   return (
+  <div>
+    <Navbar />
     <Routes>
       <Route
         path="/posts/:id"
@@ -18,6 +22,7 @@ const App = () => {
       <Route path="/login" element={<LoginForm navigate={useNavigate()} />} />
       <Route path="/signup" element={<SignUpForm navigate={useNavigate()} />} />
     </Routes>
+    </div>
   );
 };
 
